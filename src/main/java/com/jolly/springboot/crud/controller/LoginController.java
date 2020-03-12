f8b71fc352f18548379af21c377f009c720f90ca
KTaranau <ktaranau@gomel.iba.by>
2020-03-12 17:35:35 +0300

    login update

--- src/main/java/com/jolly/springboot/crud/controller/LoginController.java
+++ src/main/java/com/jolly/springboot/crud/controller/LoginController.java
@@ -1,12 +1,9 @@
 package com.jolly.springboot.crud.controller;
 
-import com.jolly.springboot.crud.entity.Employee;
 import com.jolly.springboot.crud.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller("authorization")
 public class LoginController {
