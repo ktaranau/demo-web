137da76e73cd4e3b6b2ebca9c8ad6d676dfd9355
KTaranau <ktaranau@gomel.iba.by>
2020-03-18 17:25:16 +0300

    changed properties file to yml, changed security configs finally works

--- src/main/java/com/jolly/springboot/crud/config/DemoSecurityConfig.java
+++ src/main/java/com/jolly/springboot/crud/config/DemoSecurityConfig.java
@@ -1,34 +1,50 @@
 package com.jolly.springboot.crud.config;
 
 
+import com.jolly.springboot.crud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter
 {
 
-    // add a reference to our security data source
-
     @Autowired
-    @Qualifier("securityDataSource")
-    private DataSource securityDataSource;
+    private UserService userService;
+
+//    @Autowired
+//    @Qualifier("securityDataSource")
+//    private DataSource securityDataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.jdbcAuthentication().dataSource(securityDataSource);
-
+        auth.authenticationProvider(authenticationProvider());
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -46,7 +62,10 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter
                 .loginProcessingUrl("/authenticateTheUser")
                 .permitAll()
                 .and()
-                .logout().permitAll()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .permitAll()
                 .and()
                 .exceptionHandling().accessDeniedPage("/access-denied");
     }
--- src/main/java/com/jolly/springboot/crud/entity/User.java
+++ src/main/java/com/jolly/springboot/crud/entity/User.java
@@ -8,16 +8,16 @@ import java.util.Collection;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name = "first_name")
+//    @Column(name = "first_name")
     private String firstName;
-    @Column(name = "last_name")
+//    @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email")
+//    @Column(name = "email")
     private String email;
-    @Column(name = "password")
+//    @Column(name = "password")
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
